3c1cef2d90d9e237c709bb34f9d15b7fc287d0f3
abhishektiwary01 <tiwary.abhishk@gmail.com>
2024-07-16 15:24:55 +0530

    Vite.config changed

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-// vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react()],
+plugins: [react()],
+resolve: {
+alias: {
+"@": path.resolve(__dirname, "./src"),
+},
+},
 })
-
